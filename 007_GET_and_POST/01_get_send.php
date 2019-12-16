120eb9fe7126abafb55cd92d183f7a95d1d2a5a4
kazuki19992 <kushida98@gmail.com>
2019-12-16 22:25:17 +0900

    更新

---
+++ 007_GET_and_POST/01_get_send.php
@@ -0,0 +1,19 @@
+// 送信ページ
+// GET_POST
+
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>GET送信</h1>
+        <p>お名前を入力してください</p>
+
+        <form action="get_receive.php" method="GET">
+            <label>お名前</label>
+            <input type="text" name="name">
+            <label>趣味</label>
+            <input type="text" name="hobby">
+        </form>
+    </body>
+</html>
